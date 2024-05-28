b85f5470afc9c5b10ebdb3ee723bd3fe6333b013
Haridev <haridev.8281@outlook.com>
2024-05-28 15:44:48 +0530

    first commit

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const multer = require("multer");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 
@@ -16,22 +18,47 @@ mongoose.connect("mongodb://localhost:27017/avodhaEjs");
 
 const userSchema = mongoose.Schema({
   name: String,
+  content1: String,
+  content2: String,
+  file: String,
 });
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/", (req, res) => {
-  res.render("index");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-app.post("/submit", (req, res) => {
+const upload = multer({ storage: storage });
+
+app.get("/", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.render("index", { users: users });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/submit", upload.single("file"), (req, res) => {
   const user = new User({
     name: req.body.name,
+    content1: req.body.content1,
+    content2: req.body.content2,
+    file: req.file.filename,
   });
 
   user
     .save()
-    .then((result) => res.json("User saved successfully"))
+    .then((result) => {
+      res.redirect("/");
+    })
     .catch((err) => console.log(err));
 });
 
